3d4af87ae789e1e2de68f8391ebf3f00510d6d91
NaveenKumar <Naveenk@middlewaretalents.com.com>
2025-12-28 09:17:58 +0000

    Changes on Naveen Comment

--- DemoTesting.java
+++ DemoTesting.java
@@ -7,13 +7,13 @@ public class DemoTesting
             ts.assign_values();
             ts.display_intance_variables();
 
-            System.out.println("---------------");
+            
             // call - static method
             ts.display_static_variables();
 
             // or you can call with class name also.
             // static variables and methods can be called using class name also.
-            System.out.println("---------------");
+           
             // calling static method using class name
             Testing.display_static_variables();
             // calling static varible using class name
--- Testing.java
+++ Testing.java
@@ -1,19 +1,17 @@
-public class Testing 
-{
+public class Testing {
     // instance variables
     int idno;
-    String name;    
+    String name;
     float exp;
     String address;
 
     // static variables
     static String subject_name;
-    static String time; 
+    static String time;
     static String topic;
-    
+
     // instance method
-    public void assign_values()
-    {
+    public void assign_values() {
         // intance variables
         idno = 456843;
         name = "Ravi";
@@ -27,28 +25,27 @@ public class Testing
         topic = "OOPS";
     }
 
-    //instance method
-    public void display_intance_variables()
-    {
+    // instance method
+    public void display_intance_variables() {
         // printing instance variables
         System.out.println(idno);
         System.out.println(name);
         System.out.println(exp);
-        System.out.println(address);
+        System.out.println(address); // print instance variables
 
         // printing static variables
-        // instasnce method can access static variables and methods also. 
+        // instasnce method can access static variables and methods also.
         System.out.println(subject_name);
         System.out.println(time);
         System.out.println(topic);
     }
 
     // static method
-    public static void display_static_variables()
-    {
+    public static void display_static_variables() {
         System.out.println(subject_name);
         System.out.println(time);
         System.out.println(topic);
-        // System.out.println(idno); --> You canot use instance variable directly in static method
+        // System.out.println(idno); --> You canot use instance variable directly in
+        // static method
     }
 }
